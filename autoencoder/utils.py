2380a0b62204dd80435c0744849f373f690bd6f9
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-02-18 13:59:15 +0530

    Add files via upload

--- autoencoder/utils.py
+++ autoencoder/utils.py
@@ -165,7 +165,7 @@ def setup_dataset(dataset_name):
             "model": {
                 "encoder_type": "autoenc",
                 "feature_dims": 128,
-                "learning_rate": 0.01,
+                "learning_rate": 0.001,
                 "optimizer": {
                     "type": "Adam",
                     "weight_decay": 0.0001,
