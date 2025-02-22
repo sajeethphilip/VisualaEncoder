8a1dcbd46f8690f86c4ce1ff9c7117d000992f17
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-02-23 03:17:10 +0530

    update

--- autoencoder/train.py
+++ autoencoder/train.py
@@ -6,8 +6,8 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 from autoencoder.model import Autoencoder, ModifiedAutoencoder
-from autoencoder.utils import get_device, save_latent_space, save_embeddings_as_csv, save_checkpoint, update_progress, display_confusion_matrix,ssim_loss,save_latent_space_for_epoch
-from autoencoder.utils import load_checkpoint, load_local_dataset, load_dataset_config, save_1d_latent_to_csv, save_batch_latents, display_header,update_confusion_matrix
+from autoencoder.utils import get_device, save_latent_space, save_embeddings_as_csv, save_checkpoint, update_progress, display_confusion_matrix,ssim_loss,save_latent_space_for_epoch,postprocess_hdr_image
+from autoencoder.utils import load_checkpoint, load_local_dataset, load_dataset_config, save_1d_latent_to_csv, save_batch_latents, display_header,update_confusion_matrix,preprocess_hdr_image
 from datetime import datetime
 from tqdm import tqdm
 from colorama import init, Fore, Back, Style
